1fabea3cd03c68a96f64473990207c9f8a2ef6df
Emile Fokkema <emile.fokkema@t-mobile.nl>
2017-05-24 10:34:04 +0200

    display the NRC kind

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,4 +1,8 @@
 define(["sudokuGrid","setClass"],function(sudokuGrid, setClass){
+	var kind = {
+		NRC:{className:"nrc"},
+		NORMAL:{className:""}
+	};
 	var makeCell = function(makeElement){
 		return makeElement(function(input, container){
 			var setError = function(val){
@@ -9,20 +13,57 @@ define(["sudokuGrid","setClass"],function(sudokuGrid, setClass){
 			};
 		});
 	};
-
-	return requireElement(document.getElementById("editor").innerHTML,function(div, row){
-		document.body.appendChild(div);
-		var grid = new sudokuGrid();
-
-		for(var i=0;i<9;i++){
-			row(function(cell){
-				for(j=0;j<9;j++){
-					var gridPosition;
-					gridPosition = grid.add(i, j, makeCell(cell));
+	var setKindClass = function(el, k){
+		for(var kk in kind){
+			if(kind.hasOwnProperty(kk)){
+				if(kind[kk] == k){
+					setClass(el, kind[kk].className, true);
+				}else{
+					setClass(el, kind[kk].className, false);
 				}
-			});
+			}
 		}
+	};
 
-		grid.rows[1][2].setError(true);
-	});
+	return requireElement(document.getElementById("editor").innerHTML,
+		function(
+			div,
+			row,
+			settingsButton,
+			closeSettingsButton,
+			nrc,
+			normal){
+				document.body.appendChild(div);
+				var grid = new sudokuGrid();
+				var currentKind = kind.NORMAL;
+
+				for(var i=0;i<9;i++){
+					row(function(cell){
+						for(j=0;j<9;j++){
+							var gridPosition;
+							gridPosition = grid.add(i, j, makeCell(cell));
+						}
+					});
+				}
+
+				settingsButton.addEventListener('click',function(){
+					setClass(div, "show-settings", true);
+				});
+
+				closeSettingsButton.addEventListener('click',function(e){
+					setClass(div, "show-settings", false);
+					e.cancelBubble = true;
+					return false;
+				});
+
+				nrc.addEventListener('click',function(){
+					currentKind = kind.NRC;
+					setKindClass(div, currentKind);
+				});
+
+				normal.addEventListener('click',function(){
+					currentKind = kind.NORMAL;
+					setKindClass(div, currentKind);
+				});
+			});
 })
